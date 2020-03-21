8f63bfd28da57fd847a88b5e4363596712a0ea49
Stanislav Kozin <stas@staskozin.ru>
2020-03-21 22:12:15 +0300

    Add keyboard handler

--- bot.py
+++ bot.py
@@ -10,3 +10,18 @@ bot = TeleBot(TOKEN)
 def handle_message(msg):
     sql.delete_from_queue(msg.chat.id)
     sql.insert_row('print_queue', {"user_id": msg.chat.id, "is_active": 1})
+    buttons = [('Зона 1', 'zone_id=1'), ('Я пошутил', 'no')]
+    bot.send_message(msg.chat.id, text='Зона?', reply_markup=get_inline_keyboard(buttons))
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('zone_id'))
+def handle_zone(call):
+    zone_id = int(call.data.split('=')[-1])
+    bot.send_message(call.message.chat.id, f'Добавляем зону {zone_id}')
+
+
+def get_inline_keyboard(buttons):
+    keyboard = types.InlineKeyboardMarkup()
+    for button in buttons:
+        keyboard.add(types.InlineKeyboardButton(text=button[0], callback_data=button[1]))
+    return keyboard
