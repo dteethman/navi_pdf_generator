54260622f5ad189694032e90590221acf1013f7e
dteethman <zuuuubice@gmail.com>
2020-03-23 23:26:12 +0300

    Change polling to webhook

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from bot import bot
+from telebot import types
+from flask import Flask, request
 import actions
 import pkgutil
+import os
+
+
+
 modules = []
 for _, name, _ in pkgutil.iter_modules(actions.__path__):
     modules.append(name)
 exec(f'from actions import {",".join(modules)}')
 
-bot.polling(none_stop=True, interval=0)
+server = Flask(__name__)
+
+
+@server.route('/', methods=['POST'])
+def webhook():
+    bot.process_new_updates([types.Update.de_json(request.stream.read().decode('utf-8'))])
+    return '?', 200
+
+
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+
