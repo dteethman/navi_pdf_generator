937ad42463a39f822a4add3ec72d4b09343b4db4
dteethman <zuuuubice@gmail.com>
2020-09-21 23:54:32 +0300

    Move sql

--- sql.py
+++ sql.py
@@ -24,27 +24,27 @@ def executemany(query, data):
 
 def insert_row(table: str, cols: dict):
     names = ','.join(list(cols.keys()))
-    count = ('?,' * len(cols.keys()))[:-1]
+    count = ('%s,' * len(cols.keys()))[:-1]
     values = tuple(cols.values())
     return execute(f'INSERT INTO {table} ({names}) VALUES ({count})', values)
 
 
 def update(table: str, cols: dict, where: dict):
-    set = ','.join([f'{col}=?' for col, value in cols.items()])
-    keys = ' AND '.join([f'{key}=?' for key, value in where.items()])
+    set = ','.join([f'{col}=%s' for col, value in cols.items()])
+    keys = ' AND '.join([f'{key}=%s' for key, value in where.items()])
     values = list(cols.values()) + list(where.values())
     return execute(f'UPDATE {table} SET {set} WHERE {keys}', tuple(values))
 
 
 def delete(table: str, where: str, equal):
     try:
-        return execute(f'DELETE FROM {table} WHERE {where}=?', (equal,))
+        return execute(f'DELETE FROM {table} WHERE {where}=%s', (equal,))
     except psycopg2.OperationalError as err:
         print(f'Ошибка: {err}')
 
 
 def get_active_row(user_id):
-    return execute('SELECT * FROM print_queue WHERE user_id=? AND is_active=1', (user_id,)).fetchone()
+    return execute('SELECT * FROM print_queue WHERE user_id=%s AND is_active=1', (user_id,)).fetchone()
 
 
 def get_zones() -> list:
@@ -52,46 +52,46 @@ def get_zones() -> list:
 
 
 def get_zone(zone_id) -> tuple:
-    return execute('SELECT * FROM zones WHERE id=?', (zone_id,)).fetchone()
+    return execute('SELECT * FROM zones WHERE id=%s', (zone_id,)).fetchone()
 
 
 def get_categories(zone_id: int) -> list:
     return execute(
         'SELECT id, category, display_name, print_name, is_final FROM categories '
         'JOIN categories_to_zones ON id = categories_to_zones.cat_id '
-        'WHERE categories_to_zones.zone_id = ? '
+        'WHERE categories_to_zones.zone_id = %s '
         'ORDER BY display_name',
         (zone_id,)).fetchall()
 
 
 def get_category(cat_id) -> tuple:
-    return execute('SELECT * FROM categories WHERE id=?', (cat_id,)).fetchone()
+    return execute('SELECT * FROM categories WHERE id=%s', (cat_id,)).fetchone()
 
 
 def get_brands(cat_id: int) -> list:
     return execute(
         'SELECT id, brand, display_name, is_final, is_ignored FROM brands '
         'JOIN brands_to_categories ON id = brands_to_categories.brand_id '
-        'WHERE brands_to_categories.cat_id = ? '
+        'WHERE brands_to_categories.cat_id = %s '
         'ORDER BY display_name',
         (cat_id,)).fetchall()
 
 
 def get_brand(brand_id) -> tuple:
-    return execute('SELECT * FROM brands WHERE id=?', (brand_id,)).fetchone()
+    return execute('SELECT * FROM brands WHERE id=%s', (brand_id,)).fetchone()
 
 
 def get_models(brand_id: int) -> list:
     return execute(
         'SELECT id, model FROM models '
         'JOIN models_to_brands ON id = models_to_brands.model_id '
-        'WHERE models_to_brands.brand_id = ? '
+        'WHERE models_to_brands.brand_id = %s '
         'ORDER BY model',
         (brand_id,)).fetchall()
 
 
 def get_model(model_id) -> tuple:
-    return execute('SELECT * FROM models WHERE id=?', (model_id,)).fetchone()
+    return execute('SELECT * FROM models WHERE id=%s', (model_id,)).fetchone()
 
 
 def add_to_queue(cols: dict, user_id):
@@ -100,13 +100,13 @@ def add_to_queue(cols: dict, user_id):
 
 def delete_from_queue(user_id):
     try:
-        return execute(f'DELETE FROM print_queue WHERE user_id=? AND is_active=1', (user_id,))
+        return execute(f'DELETE FROM print_queue WHERE user_id=%s AND is_active=1', (user_id,))
     except psycopg2.OperationalError as err:
         print(f'Ошибка: {err}')
 
 
 def get_from_queue_by_id(queue_id):
-    return execute('SELECT * FROM print_queue WHERE id=?', (queue_id,)).fetchone()
+    return execute('SELECT * FROM print_queue WHERE id=%s', (queue_id,)).fetchone()
 
 
 def clear_queue(user_id):
@@ -114,7 +114,7 @@ def clear_queue(user_id):
 
 
 def get_queue(user_id):
-    return execute('SELECT * FROM print_queue WHERE user_id=? ', (user_id,)).fetchall()
+    return execute('SELECT * FROM print_queue WHERE user_id=%s ', (user_id,)).fetchall()
 
 
 if __name__ == '__main__':
